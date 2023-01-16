72e11fec3cf0355d5cf85720186fabbf57126edd
Triska Mahfud K <triska.triskacode@gmail.com>
2023-01-16 14:39:13 +0700

    initial commit

---
+++ src/ui/index.tsx
@@ -0,0 +1,13 @@
+import "./styles/index.css";
+import React from "react";
+import App from "./pages/app";
+
+const Index: React.FC = () => {
+  return (
+    <React.Fragment>
+      <App />
+    </React.Fragment>
+  );
+};
+
+export default Index;
---
+++ src/ui/pages/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-semibold text-slate-500">Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
